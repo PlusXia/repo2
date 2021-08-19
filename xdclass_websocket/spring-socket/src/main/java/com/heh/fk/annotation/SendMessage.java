8602df53751bcfbcfcc66b8266c544f0fdd4e573
zql <xiayanheng@163.com>
2021-08-19 10:31:32 +0800

    添加到本地仓库

--- xdclass_websocket/spring-socket/src/main/java/com/heh/fk/annotation/SendMessage.java
+++ xdclass_websocket/spring-socket/src/main/java/com/heh/fk/annotation/SendMessage.java
@@ -10,6 +10,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 public @interface SendMessage {
 
-    String topic() default "/subscribe";
+    String topic() default "/subscribe3";
 
 }
