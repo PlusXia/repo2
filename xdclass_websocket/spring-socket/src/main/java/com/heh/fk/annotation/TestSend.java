d157962e981dd80197d0b7144e7c6afd34436b12
zql <xiayanheng@163.com>
2021-08-19 11:14:13 +0800

    12

--- xdclass_websocket/spring-socket/src/main/java/com/heh/fk/annotation/TestSend.java
+++ xdclass_websocket/spring-socket/src/main/java/com/heh/fk/annotation/TestSend.java
@@ -10,5 +10,5 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 public @interface TestSend {
 	
-	 String topic() default "/subscribe";
+	 String topic() default "/subscribe222";
 }
